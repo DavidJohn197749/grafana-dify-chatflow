2ac011c83bbb14509bf53cebc3024936fb1e742b
DavidJohn197749 <40290664+DavidJohn197749@users.noreply.github.com>
2025-10-10 13:44:52 +0000

    feat: add handleDifyWorkflow endpoint for processing API requests

--- pkg/plugin/resources.go
+++ pkg/plugin/resources.go
@@ -3,6 +3,9 @@ package plugin
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 )
 
 // handlePing is an example HTTP GET resource that returns a {"message": "ok"} JSON response.
@@ -37,8 +40,47 @@ func (a *App) handleEcho(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func (a *App) handleDifyWorkflow(w http.ResponseWriter, req *http.Request) {
+	pluginConfig := backend.PluginConfigFromContext(req.Context())
+	jsonData := pluginConfig.AppInstanceSettings.JSONData
+	secureJsonData := pluginConfig.AppInstanceSettings.DecryptedSecureJSONData
+	log.DefaultLogger.Debug("secureJsonData content", "data", secureJsonData)
+	log.DefaultLogger.Debug("jsonData content", "data", jsonData)
+	log.DefaultLogger.Debug("Goto SLS with STS success.")
+
+	var config map[string]interface{}
+	if err := json.Unmarshal(jsonData, &config); err != nil {
+		http.Error(w, "Invalid JSONData", http.StatusInternalServerError)
+		return
+	}
+	apiUrl, ok := config["apiUrl"].(string)
+	if !ok {
+		http.Error(w, "apiUrl not found or not a string", http.StatusBadRequest)
+		return
+	}
+	
+	apiKey, ok := secureJsonData["apiKey"]
+	if !ok  {
+		http.Error(w, "API key is not set", http.StatusBadRequest)
+		return
+	}else {
+		
+		response := map[string]string{
+			"apiKey": apiKey,
+			"apiUrl": apiUrl,
+		}
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
 // registerRoutes takes a *http.ServeMux and registers some HTTP handlers.
 func (a *App) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("/ping", a.handlePing)
 	mux.HandleFunc("/echo", a.handleEcho)
+	mux.HandleFunc("/difyWorkflow", a.handleDifyWorkflow)
 }
